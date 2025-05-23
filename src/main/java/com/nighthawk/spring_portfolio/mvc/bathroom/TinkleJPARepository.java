8a5d2286c9d7964dcf33b0631e9e0818783a9e59
Lilian Wu <lilianwu08@gmail.com>
2025-05-23 12:32:00 -0700

    tinkle bulk clear

--- src/main/java/com/nighthawk/spring_portfolio/mvc/bathroom/TinkleJPARepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/bathroom/TinkleJPARepository.java
@@ -2,9 +2,18 @@ package com.nighthawk.spring_portfolio.mvc.bathroom;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import jakarta.transaction.Transactional; 
 
 public interface TinkleJPARepository extends JpaRepository<Tinkle, Long> {
     // Optional<Tinkle> findByStudentEmail(String studentEmail);'
     Optional<Tinkle> findByPersonName(String personName);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Tinkle")
+    void clearTinkleTable();
 }
